e5d02e22684ee23246b705fee076c42d72c35ac8
doubleblindpolicy <92842452+doubleblindpolicy@users.noreply.github.com>
2023-02-02 18:05:50 +0800

    Add files via upload

---
+++ GCoT_run.sh
@@ -0,0 +1,30 @@
+GPU=0
+seed=31  # 31 32 33 34
+code=main_imp.py 
+
+data_dir=/home/xym/KDD2023/DisC/data/
+dataset=MNIST_75sp_0.8 # MNIST_75sp_0.9 MNIST_75sp_0.95 fashion_0.8 fashion_0.9 fashion_0.95 kuzu_biased_0.8 kuzu_biased_0.9 kuzu_biased_0.95
+all_epochs=300
+use_mask=1
+swap_epochs=150
+lambda_swap=15
+lambda2=1
+q=0.7
+lambda_dis=1
+
+
+str1="output_GCN_"$dataset"_seed_"$seed"_lambda_swap_"$lambda_swap"_lambda2_"$lambda2"_CrossNorm"
+python -u $code --config 'configs/superpixels_graph_classification_GCoT_MNIST_100k.json' \
+--dataset $dataset \
+--data_dir $data_dir \
+--seed $seed  \
+--mask_epochs $all_epochs \
+--swap_epochs $swap_epochs \
+--lambda_swap $lambda_swap \
+--lambda2 $lambda2 \
+--use_mask $use_mask \
+--q $q \
+--lambda_dis $lambda_dis \
+--out_dir $str1 \
+--gpu_id $GPU
+
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# GCoT
-Out-of-Distribution Generalization Graph Neural Network via Chain-of-Thought Causal Reasoning
+# Out-of-Distribution Generalization Graph Neural Network via Chain-of-Thought Causal Reasoning
+
+## Datasets
+
+| **Dataset** | # train/val/test |  #  bias degree  | # classes |
+| :---------: | :--------: | :-----: | :--------: |
+|  **CMNIST-75sp**   |     10K/5K/10K      | 0.8/0.9/0.95 |     10      |
+|  **CFashion-75sp**   |     10K/5K/10K      | 0.8/0.9/0.95 |     10      |
+|  **CKuzushiji-75sp**   |     10K/5K/10K      | 0.8/0.9/0.95 |     10      |
+
+
+## Usage
+```python
+sh GCoT_run.sh
+```
+
+## Dependencies
+
+- Python 3.7
+- PyTorch 1.12.0
+- dgl-cu116 0.9.1.post1
